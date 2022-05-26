d3929154d990eeedf4035929baab5974259459ae
Paola Guerra <paola@192.168.1.10>
2022-05-26 17:53:45 -0300

    primer intento

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,20 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x < y) {
+    return y;
+  } else if (y < x) {
+    return x;
+  } else return x || y;
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad => 18){
+    return 'Allowed';
+  } else return 'Not allowed';
 }
   
 function conection(status) {
@@ -19,6 +27,12 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+
+    if (status === 1){
+      return 'Online';
+    } else if ( status === 2){
+      return 'Away';
+    } else return 'Offline';
 }
 
 function saludo(idioma) {
@@ -28,6 +42,13 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+    if (idioma = aleman){
+      return 'Guten Tag!';
+    } else if (idioma = mandarin) {
+      return 'Ni hao!';
+    } else if (idioma = ingles) {
+      return 'Hello!';
+    } else return 'Hola!';
 }
 
 function colors(color) {
@@ -44,12 +65,18 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero ===5){
+    return true;
+  } else return false;
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+    if ( numero < 50 && numero > 20){
+      return true;
+    } else return false;
 }
 
 function esEntero(numero) {
@@ -60,6 +87,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+    
 }
 
 function fizzBuzz(numero) {
@@ -67,6 +95,13 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+    if (numero % 3 === 0 && numero % 5 === 0){
+      return 'fizzbuzz';
+    } else if ( numero % 5 === 0){
+      return 'buzz';
+    }else if (numero % 3 ===0){
+      return 'fizz';
+    } else return numero;
 }
 
 function operadoresLogicos(num1, num2, num3) {
